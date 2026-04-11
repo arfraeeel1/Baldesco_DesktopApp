1ac1d2401816fc473cc6584bc93882bf9956cdd1
arfraeeel1 <20241506@nbsc.edu.ph>
2026-04-11 21:49:37 +0800

    Added Edited "GradientPanel and RoundedButton"

--- GradientPanel.java
+++ GradientPanel.java
@@ -1,16 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
+import javax.imageio.ImageIO;
 
-public class GradientPanel extends JPanel {
-    private Color color1 = new Color(245, 245, 220); // beige
-    private Color color2 = new Color(30, 144, 255);  // blue
+class GradientPanel extends JPanel {
 
+    private Image bgImage;
+
+    public GradientPanel() {
+        try {
+            URL url = new URL("https://media.tenor.com/2uyENRmiUt0AAAAd/anime-aesthetic.gif"); // Sung Jin-Woo
+            // URL url = new URL("https://media.tenor.com/2uyENRmiUt0AAAAd/anime-aesthetic.gif"); // Jujutsu
+
+            bgImage = ImageIO.read(url);
+            System.out.println("Anime background loaded!");
+        } catch (Exception e) {
+            System.out.println("Failed to load image.");
+        }
+    }
     protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (bgImage != null) {
+            g.drawImage(bgImage, 0, 0, getWidth(), getHeight(), this);
+        }
+
         Graphics2D g2 = (Graphics2D) g;
-        int width = getWidth();
-        int height = getHeight();
-        GradientPaint gp = new GradientPaint(0, 0, color2, width, height, color1);
-        g2.setPaint(gp);
-        g2.fillRect(0, 0, width, height);
+        g2.setColor(new Color(0, 0, 0, 180));
+        g2.fillRect(0, 0, getWidth(), getHeight());
+
+        g2.setColor(new Color(120, 0, 255, 80));
+        g2.fillOval(getWidth()/4, getHeight()/4, 300, 300);
     }
 }
--- RoundedButton.java
+++ RoundedButton.java
@@ -1,21 +1,29 @@
-import javax.swing.*;
-import java.awt.*;
 
-public class RoundedButton extends JButton {
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JButton;
+
+class RoundedButton extends JButton {
+
     public RoundedButton(String text) {
         super(text);
         setFocusPainted(false);
-        setBackground(new Color(30, 144, 255));
         setForeground(Color.WHITE);
-        setFont(new Font("Arial", Font.BOLD, 14));
-        setBorderPainted(false);
+        setFont(new Font("Segoe UI", Font.BOLD, 14));
+        setCursor(new Cursor(Cursor.HAND_CURSOR));
+        setContentAreaFilled(false);
     }
 
     protected void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setColor(getBackground());
-        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
-        super.paintComponent(g2);
-        g2.dispose();
+        Graphics2D g2 = (Graphics2D) g;
+
+        // Glow effect
+        g2.setColor(new Color(120, 0, 255));
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 25, 25);
+
+        super.paintComponent(g);
     }
 }
